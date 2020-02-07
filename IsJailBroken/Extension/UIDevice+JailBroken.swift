fe023773c3ff82a3a596cd4998c2d4efdaf2fc51
Vineet Choudhary <vineetchoudhary@live.in>
2020-02-07 17:09:58 +0530

    initial commit

---
+++ IsJailBroken/Extension/UIDevice+JailBroken.swift
@@ -0,0 +1,77 @@
+//
+//  UIDevice+JailBroken.swift
+//  IsJailBroken
+//
+//  Created by Vineet Choudhary on 07/02/20.
+//  Copyright © 2020 Developer Insider. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIDevice {
+    var isSimulator: Bool {
+        return TARGET_OS_SIMULATOR != 0
+    }
+    
+    var isJailBroken: Bool {
+        get {
+            if UIDevice.current.isSimulator { return false }
+            if JailBrokenHelper.isContainsSuspiciousFiles() { return true }
+            return JailBrokenHelper.canEditSystemFiles()
+        }
+    }
+}
+
+private struct JailBrokenHelper {
+    static func isContainsSuspiciousFiles() -> Bool {
+        for path in pathsToCheck {
+            if FileManager.default.fileExists(atPath: path) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    static func canEditSystemFiles() -> Bool {
+        let jailBreakText = "Developer Insider"
+        do {
+            try jailBreakText.write(toFile: jailBreakText, atomically: true, encoding: .utf8)
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    /**
+     Add more paths here to check for jail break
+     */
+    static var pathsToCheck: [String] {
+        return ["/Applications/Cydia.app",
+                "/Applications/blackra1n.app",
+                "/Applications/FakeCarrier.app",
+                "/Applications/Icy.app",
+                "/Applications/IntelliScreen.app",
+                "/Applications/MxTube.app",
+                "/Applications/RockApp.app",
+                "/Applications/SBSettings.app",
+                "/Applications/WinterBoard.app",
+                "/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
+                "/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
+                "/private/var/lib/apt",
+                "/private/var/lib/apt/",
+                "/private/var/lib/cydia",
+                "/private/var/mobile/Library/SBSettings/Themes",
+                "/private/var/stash",
+                "/private/var/tmp/cydia.log",
+                "/System/Library/LaunchDaemons/com.ikey.bbot.plist",
+                "/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
+                "/usr/bin/sshd",
+                "/usr/libexec/sftp-server",
+                "/usr/sbin/sshd",
+                "/etc/apt",
+                "/bin/bash",
+                "/Library/MobileSubstrate/MobileSubstrate.dylib"
+        ]
+    }
+}
